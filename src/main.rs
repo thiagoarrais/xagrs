cac5094beca96fd50ca74c95c4a9c12c1d8ea5ee
Thiago Arrais <thiago.arrais@gmail.com>
2020-12-14 13:20:55 -0300

    works for single-line input

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::process::Command;
 use std::env;
+use std::io::{self, BufRead};
+use std::iter;
 
 fn main() {
+    let stdin = io::stdin();
     let mut args = env::args().skip(1);
     let command_opt = args.next();
     if let Some(command) = command_opt {
-        Command::new(command)
-            .args(args)
+        let line = stdin.lock().lines().next().unwrap().unwrap();
+        Command::new(&command)
+            .args(args.chain(iter::once(line)))
             .spawn()
             .expect("something went wrong");
     }
