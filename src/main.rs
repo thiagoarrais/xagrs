d723cf3cf786dbdc849a762b8947f0afb1df396c
Thiago Arrais <thiago.arrais@gmail.com>
2020-12-18 13:58:21 -0300

    parse line limit from command line

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,54 @@
 use std::process::Command;
-use std::env;
-use std::io::{self, BufRead};
-use std::iter;
+use std::io::{self, BufRead, Error};
 
-fn main() {
+use itertools::{ Itertools, IntoChunks };
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "xagrs")]
+struct Opt {
+    #[structopt(short = "L", default_value = "1")]
+    limit: usize,
+
+    // #[structopt(short = "L", default_value = "1")]
+    command_args: Vec<String>,
+}
+
+fn chunk_lines<L>(limit: usize, lines: L) -> IntoChunks<L>
+  where L: Iterator
+{
+    lines.chunks(limit)
+}
+
+fn main() -> Result<(), Error> {
     let stdin = io::stdin();
-    let mut args = env::args().skip(1);
-    let command_opt = args.next();
-    if let Some(command) = command_opt {
-        let command_args : Vec<String> = args.collect();
-        for line in stdin.lock().lines() {
+    let mut opt = Opt::from_args();
+    if opt.command_args.len() > 0 {
+        let command = opt.command_args.remove(0);
+        for chunk in &chunk_lines(opt.limit, stdin.lock().lines()) {
+            // TODO: is it OK to unwrap?
+            let input = chunk.into_iter().map(|s| s.unwrap().to_owned());
             Command::new(&command)
-                .args(command_args.iter().chain(iter::once(&line.unwrap())))
-                .spawn()
-                .expect("something went wrong");
+                .args(opt.command_args.clone().into_iter().chain(input))
+                .spawn()?;
         }
     }
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_chunk_lines_for_command() {
+        let input = vec!["one", "two", "three"];
+        let chunked = &chunk_lines(2, input.into_iter());
+        let mut output = chunked.into_iter();
+
+        assert_eq!(output.next().unwrap().collect::<Vec<&str>>(), vec!("one", "two"));
+        assert_eq!(output.next().unwrap().collect::<Vec<&str>>(), vec!("three"));
+        assert!(output.next().is_none());
+    }
+
 }
